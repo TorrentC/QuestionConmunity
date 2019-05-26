da7dbd2c1a70e7cb8916d5a05b6ae1d2d5aa1e54
TorrentC <1783968112@qq.com>
2019-05-26 22:48:59 +0800

    use bootstrap

---
+++ readme.md
@@ -0,0 +1,3 @@
+## this is a spring boot projject
+
+#### 现在已经使用bootstrap写了一个简单的登录页面
---
+++ src/main/java/com/itstudy/studyboot/StudybootApplication.java
@@ -0,0 +1,13 @@
+package com.itstudy.studyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybootApplication.class, args);
+    }
+
+}
